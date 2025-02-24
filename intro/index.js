2b693b8e8051b03a64843ab70858424f707891b4
Ankit Tayal <tayalankit20@gmail.com>
2025-02-24 23:36:08 +0530

    additional commit Feb 2025

--- intro/index.js
+++ intro/index.js
@@ -69,6 +69,140 @@ var x = true || false;
 console.log(x)
 
 var x = "23" == 23;
+// == is an abstract comparison, it will try to make type conversion , before running
+console.log(x)
+var x = "23" === 23;
 console.log(x)
 
-// 5:22
+// TERNARY operator
+var x;
+if (truthy) {
+    x = 1
+} else {
+    x = 2;
+}
+
+var x = truthy ? 1 : 2;
+
+// SWITCH
+var expression = 'dog';
+switch (expression) {
+    case 'dog':
+        console.log('🐕');
+        break;
+
+    case 'cat':
+        console.log('🐈');        
+        break;
+}
+
+// TRY/CATCH
+try {
+    // throw new Error();
+    console.log('works');    
+} catch (error) {
+    console.log('broke');
+} finally {
+    console.log('always works');
+}
+
+// 4. VARIABLES
+x = '😎';
+
+var x = 'something';
+x = 'something else';
+
+var g = 'global';
+function app() {
+    // var l = 'local';
+    l = 'local';  // undeclared variable
+    console.log(g);
+}
+// console.log(l);  // l is not defined
+app();
+
+// HOISTING
+console.log(x);
+var x  // will be considered , on top of code
+
+var x = 'a';
+
+function app() {
+    if (true) {
+        var x = 'b';
+    }
+
+    var x = 'c';
+}
+
+if (true) {
+    // block
+    let xm = 23;  // let is block scoped, limeted to that block, doesn't leak to parent scope (global)
+
+    const x = 23;
+    // x = 24;  // can't reassign . Assignment to constant variable.
+    console.log(xm);
+    console.log(x);
+    
+}
+// console.log(xm); // xm is not defined
+
+
+// 5. FUNCTIONS
+function hello(input) {
+    const output = 'hello ' + input + '😍';
+    return output;
+}
+console.log(hello('hi jeff'));
+
+// const hello = (input) => output
+
+// anonymous and named
+function someName() {
+    
+}
+
+const someNamee = function() {
+    
+}
+
+// higher order functions
+function cool(fun) {
+    fun();
+}
+cool(() => console.log('sweet!'));
+
+// function outer() {
+//     function inner() {
+        
+//     }
+// }
+
+// closures
+function outer() {
+
+    const fish = '🐟';
+    let count = 0;
+
+    function inner() {
+        count++;
+        return '${count} ${fish}'
+    }
+
+    return inner;
+}
+
+const fun = outer();
+console.log(fun());
+console.log(fun());
+console.log(fun());
+console.log(fun());
+
+// 10:53
+
+
+
+
+
+
+
